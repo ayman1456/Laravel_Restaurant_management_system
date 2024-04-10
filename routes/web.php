27ff2a812ec9fe408a28ae3dbe09fbb7c1fa0b44
MacBook <macbook@Niloys-MacBook-Pro.local>
2024-04-10 15:55:18 +0600

    auth done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+Route::get('/dashboard', [DashboardController::class, 'home'])->name('dashboard');
