7dd7f3295f6bba187ab43a3b8c1faa5dab29d93b
shourab <shourab.cit.bd@gmail.com>
2024-04-24 23:36:35 +0600

    login bug

--- resources/views/frontend/Auth/Login.blade.php
+++ resources/views/frontend/Auth/Login.blade.php
@@ -1,29 +1,34 @@
 @extends('layouts.frontendApp')
 @section('content')
 
-   
 
-    <!--#f7ad1d-->
-            <div class="container">
-                <div class="Title">
-                    <h1 style="color:#f7ad1d">Login</h1>
-                </div>
-                <div class="card-body" >
-                    <form action="{{ route('user.login.verify') }}" method="POST">
-                        @csrf
-                        <input type="text"  placeholder="Email" name="email" 
+
+<!--#f7ad1d-->
+<div class="container">
+    <div class="col-lg-5 mx-auto mt-5">
+        <div class="Title text-start">
+            <h1 style="color:#f7ad1d">Login</h1>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('user.login.verify') }}" method="POST">
+                @csrf
+                <div class="my-2">
+                    <input type="text" placeholder="Email" name="email"
                         style="background-color:black; border:1px solid #6A6A6A; width:400px; border-radius:10px;padding:5px">
-                        @error('email')
-                        
-                        <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                        <input type="text"  placeholder="password" name="password"
-                        style="background-color:black; border:1px solid #6A6A6A; width:400px; border-radius:10px;padding:5px">
-                        <button style="background-color: #f7ad1d;color:black;padding:5px 20px 5px 20px; border:0px;border-radius:10px">Login</button>
-                    </form>
                 </div>
-     
-            </div>
+                @error('email')
+                <span class="text-danger">{{ $message }}</span>
+                @enderror
+                <div class="my-2">
+                    <input type="text" placeholder="password" name="password"
+                        style="background-color:black; border:1px solid #6A6A6A; width:400px; border-radius:10px;padding:5px">
+                </div>
+                <button class="my-2"
+                    style="background-color: #f7ad1d;color:black;padding:5px 20px 5px 20px; border:0px;border-radius:10px">Login</button>
+            </form>
+        </div>
+    </div>
+
+</div>
 
 @endsection
-
