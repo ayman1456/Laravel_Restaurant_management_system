087cbe4ec2e5a0ccf1ff32d2bcac43875bc28ccc
shourab <shourab.cit.bd@gmail.com>
2024-05-12 22:12:59 +0600

    dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Food;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Table;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     function home()
     {
-        return view('backend.dashboard');
+        $revenew = Order::where('status', 'Complete')->orWhere('status', 'Processing')->sum('total_price');
+        $categories = Category::count();
+        $tables = Table::count();
+        $food = Food::count();
+        $employees = User::count();
+        return view('backend.dashboard', compact('revenew', 'categories', 'tables', 'food', 'employees'));
     }
 }
